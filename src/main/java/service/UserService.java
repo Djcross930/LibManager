49c7320b1412ebba63cc809058e1951c697cca1b
Dan Cross <Dancross96@gmail.com>
2023-07-26 10:29:09 -0500

    added methods into user service to do other basic operations

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -24,5 +24,21 @@ public class UserService {
         return userRepository.findAll();
     }
 
+    public User getUser(Long id) {
+        return userRepository.findById(id).orElse(null);  // Returns null if the user does not exist
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    public User updateUser(User user) {
+        // In this case, the id of the user passed to this method should be set to the id of the user you want to update.
+        // If the user exists, the existing user will be updated with the fields of the passed user.
+        // If the user does not exist, a new user will be created with the fields of the passed user.
+        return userRepository.save(user);
+    }
+
     // You can continue to add more methods here based on your application needs...
 }
+
